65b506746e8c3f16563b2491f56bbf69052adcb3
kanish Kumar <kanishkumar11@outlook.in>
2026-01-17 17:34:39 +0530

    feat: Implement initial authentication pages (login, signup) with a shared layout and global styling.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,39 +1,7 @@
 import { LoginForm } from "@/features/auth/components/LoginForm";
 import { requireUnauth } from "@/lib/auth-utils";
-import React from "react";
 
 export default async function LoginPage() {
   await requireUnauth();
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-6 md:p-10 bg-zinc-50 dark:bg-zinc-950">
-      <div className="w-full max-w-sm flex flex-col gap-6">
-        <a
-          href="/"
-          className="flex items-center gap-2 self-center font-bold text-xl mb-4"
-        >
-          <div className="flex h-8 w-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="size-5"
-            >
-              <path d="M17 3H7"></path>
-              <path d="M20 21H4"></path>
-              <path d="M22 6.5C22 4.01472 19.9853 2 17.5 2C15.0147 2 13 4.01472 13 6.5C13 8.98528 15.0147 11 17.5 11C18.1726 11 18.8078 10.8524 19.3791 10.5849L22 13L21 21"></path>
-              <path d="M17.5 21L14.5 9"></path>
-            </svg>
-          </div>
-          FlowForge
-        </a>
-        <LoginForm />
-      </div>
-    </div>
-  );
+  return <LoginForm />;
 }
